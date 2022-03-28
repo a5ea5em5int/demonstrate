075fa1c1ed2cc96e38ae4c7b2a08375d39d50057
40012435 <a.myint@napier.ac.uk>
2022-03-28 15:01:33 +0630

    Subtraction from featureSubtraction added

---
+++ src/main/java/com/demo/Subtraction.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class Subtraction {
+
+    float c;
+    float d;
+
+    public float getC() {
+        return c;
+    }
+
+    public float getD() {
+        return d;
+    }
+
+    public Subtraction(float c, float d) {
+        this.c = c;
+        this.d = d;
+    }
+}
