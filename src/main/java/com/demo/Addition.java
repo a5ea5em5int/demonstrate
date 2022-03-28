eb84c0d47a971f1ab1e3d8fd0230d4d436bd9591
40012435 <a.myint@napier.ac.uk>
2022-03-28 14:59:00 +0630

    POM and Addtion class added

---
+++ src/main/java/com/demo/Addition.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+public class Addition {
+    int a;
+    int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public Addition(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
