50f00f0490879c42de8557fefd70e18b7b447eeb
40012435 <a.myint@napier.ac.uk>
2022-03-28 16:16:51 +0630

    division class is added

---
+++ src/main/java/com/demo/Division.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+public class Division {
+
+    int k,j;
+
+    public Division(int k, int j) {
+        this.k = k;
+        this.j = j;
+    }
+
+    public int getK() {
+        return k;
+    }
+
+    public int getJ() {
+        return j;
+    }
+}
